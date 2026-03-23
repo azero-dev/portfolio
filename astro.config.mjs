d8055a14ff1ef320dc247720d5df49124d8cb035
F Rodriguez <fr.rodmar@gmail.com>
2026-03-23 20:33:23 +0000

    Complete Phase 8: RSS, Sitemap and SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,23 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [
+    react(),
+    tailwind(),
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en-GB',
+          es: 'es',
+        },
+      },
+    }),
+  ],
   adapter: cloudflare({
     imageService: 'cloudflare-binding',
     imagesBindingName: 'IMAGES',
